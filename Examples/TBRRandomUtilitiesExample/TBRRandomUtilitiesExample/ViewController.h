2efc923c83db58179f55c97876e66f6ba4b810a9
Luciano Marisi <lucianomarisi@gmail.com>
2014-07-11 11:05:58 +0100

    Added example project

---
+++ Examples/TBRRandomUtilitiesExample/TBRRandomUtilitiesExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TBRRandomUtilitiesExample
+//
+//  Created by Luciano Marisi on 11/07/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Examples/TBRRandomUtilitiesExample/TBRRandomUtilitiesExampleTests/TBRRandomUtilitiesExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  TBRRandomUtilitiesExampleTests.m
+//  TBRRandomUtilitiesExampleTests
+//
+//  Created by Luciano Marisi on 11/07/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TBRRandomUtilitiesExampleTests : XCTestCase
+
+@end
+
+@implementation TBRRandomUtilitiesExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
